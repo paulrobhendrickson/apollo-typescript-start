5d0735ce70f4906a61e111cb2379ff62af4a8447
Paul Hendrickson <paul_hendrickson@crgl-thirdparty.com>
2020-11-10 09:42:53 -0600

    updating port to include environment variables

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { schema } from "./api/schema";
 
 const server = new ApolloServer({ schema });
 
-server.listen({ port: 4000 }, () =>
-  console.log("Now browse to http://localhost:4000" + server.graphqlPath)
+const port = process.env.PORT || 4000;
+
+server.listen({ port }, () =>
+  console.log("Now browse to http://localhost:" + port)
 );
